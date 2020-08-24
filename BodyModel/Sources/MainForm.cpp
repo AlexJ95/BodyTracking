bd9981d9e3affdf12aa864d321283fc860ca96bd
Sanel <hasic.sanel@gmx.de>
2020-08-24 13:46:31 +0200

    Add MainWindow

--- BodyModel/Sources/MainForm.cpp
+++ BodyModel/Sources/MainForm.cpp
@@ -1,35 +1,113 @@
 #include "MainForm.h"
 
 
-MainForm::MainForm() 
-{
+
+MainForm::MainForm() {
+	levels.push_back("Level 0");
+	levels.push_back("Level 1");
+	levels.push_back("Level 2");
+	levels.push_back("Level 3");
 };
+
 void MainForm::drawForm()
 {
+	ImGuiIO io = ImGui::GetIO();
+	ImVec2 contentSize = io.DisplaySize;
+		
 	///////First Window//////////////////
-	ImGui::Begin("Test");
-
-	ImGui::Text("This is an example Text.");
-	if (ImGui::Button("Click me"))
-		counter += 1;
-	std::string clickCount = "Click Count " + std::to_string(counter);
-	ImGui::Text(clickCount.c_str());
-	ImGui::Checkbox("Another Window", &show_another_window);
-	ImGui::End();
-	///////First Window End///////////////
-
-	///////second Window//////////////////
-
-
-	///////Second Window End//////////////
-	if (show_another_window)
+	//ImGui Flags in imgui.h
+	if (!show_setting_window & !show_level_window)
 	{
-		ImGui::Begin("Another Window", &show_another_window);
-		ImGui::Text("Hello from another Window");
-		if (ImGui::Button("Close Me"))
-			show_another_window = false;
+		ImGui::Begin("Menu", 0, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::SetWindowSize("Menu", ImVec2(400, 100), 0);
+		ImVec2 wPos(contentSize.x * 0.5f - ImGui::GetWindowWidth() * 0.5f, contentSize.y * 0.3f - ImGui::GetWindowHeight() * 0.5f);
+		ImGui::SetWindowPos(wPos);
+
+		ImGui::SetCursorPosX(ImGui::GetWindowSize().x * 0.1f);
+		if (ImGui::Button("Setting", ImVec2(ImGui::GetWindowSize().x * 0.8f, 0.0f)))
+			show_setting_window = true;
+
+		ImGui::SetCursorPosX(ImGui::GetWindowSize().x * 0.1f);
+		if (ImGui::Button("Choose Level", ImVec2(ImGui::GetWindowSize().x * 0.8f, 0.0f)))
+			show_level_window = true;
+
+		ImGui::SetCursorPosX(ImGui::GetWindowSize().x * 0.1f);
+		if (ImGui::Button("Quit", ImVec2(ImGui::GetWindowSize().x * 0.8f, 0.0f)))
+		{
+			
+			messageBoxOpen = true;
+		}
+			
+
 		ImGui::End();
+
+		if (messageBoxOpen)
+		{
+			ImGui::Begin("Quit Game", &messageBoxOpen, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+			ImGui::SetWindowSize("Quit Game", ImVec2(200, 80), 0);
+			ImVec2 wPos(contentSize.x * 0.5f - ImGui::GetWindowWidth() * 0.5f, contentSize.y * 0.3f - ImGui::GetWindowHeight() * 0.5f);
+			ImGui::SetWindowPos(wPos);
+			ImGui::Text("Are you sure?");
+			
+			ImGui::SetCursorPosY(ImGui::GetWindowSize().y - 20 - ImGui::GetWindowSize().y * 0.1f);
+			ImGui::SetCursorPosX(ImGui::GetWindowSize().x * 0.1f);
+			if (ImGui::Button("Quit", ImVec2(60, 20)))
+			{
+				Kore::System::stop();
+			}
+			ImGui::SameLine();
+			ImGui::SetCursorPosX(ImGui::GetWindowSize().x-60-ImGui::GetWindowSize().x * 0.1f);
+			if (ImGui::Button("Cenecel", ImVec2(60, 20)))
+			{
+				messageBoxOpen = false;
+			}
+			ImGui::End();
+		}
 	}
 
+	///////Second Window End//////////////
+	if (show_setting_window)
+	{
+		ImGui::Begin("Setting", &show_setting_window, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::SetWindowSize("Setting", ImVec2(400, 300), 0);
+		ImVec2 wPos(contentSize.x * 0.5f - ImGui::GetWindowWidth() * 0.5f, contentSize.y * 0.3f - ImGui::GetWindowHeight() * 0.5f);
+		ImGui::SetWindowPos(wPos);
+		ImGui::Checkbox("Audio", &aduio_On);
+		ImGui::Text("Speed:");
+		static int e =0;
+		ImGui::RadioButton("50%", &e, 0); ImGui::SameLine();
+		ImGui::RadioButton("75%", &e, 1); ImGui::SameLine();
+		ImGui::RadioButton("100%", &e, 2);
+		if (e == 0)
+			speed = 50.f;
+		else if (e == 1)
+			speed = 75.f;
+		else
+			speed = 100.f;
+
+		ImGui::SetCursorPosX(ImGui::GetWindowSize().x - 90);
+		ImGui::SetCursorPosY(ImGui::GetWindowSize().y - 50);
+		if (ImGui::Button("Close", ImVec2(70, 30)))
+		{
+			show_setting_window = false;
+		}
+	}
+	else if (show_level_window)
+	{
+		ImGui::Begin("Levels", &show_level_window, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::SetWindowSize("Levels", ImVec2(400, 300), 0);
+		ImVec2 wPos(contentSize.x * 0.5f - ImGui::GetWindowWidth() * 0.5f, contentSize.y * 0.3f - ImGui::GetWindowHeight() * 0.5f);
+		ImGui::SetWindowPos(wPos);
+	
+		ImGui::ListBox("", &currentLevel,levels.data(), levels.size());
+
+		ImGui::SetCursorPosX(ImGui::GetWindowSize().x - 90);
+		ImGui::SetCursorPosY(ImGui::GetWindowSize().y - 50);
+		if (ImGui::Button("Close",ImVec2(70,30)))
+		{
+			show_level_window = false;
+		}
+		ImGui::End();
+	}
 	////////////////////////////////////////////////////////////
 };
--- BodyModel/Sources/MainForm.h
+++ BodyModel/Sources/MainForm.h
@@ -1,11 +1,21 @@
 #pragma once
 #include "Form.h"
+#include <vector>
+
 class MainForm : public Form
 {
 private:
-	int counter = 0;
 	bool show_demo_window = true;
-	bool show_another_window = false;
+	bool show_setting_window = false;
+	bool show_level_window = false;
+	bool messageBoxOpen = false;
+	//Setting Flags
+	bool aduio_On = true;
+	float speed = 100.f;
+
+	//Level Flags
+	std::vector<char*> levels;
+	int currentLevel = 0;
 public:
 	MainForm();
 	void drawForm();
